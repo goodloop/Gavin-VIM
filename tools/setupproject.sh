47eefd93e3cc928b49d9edf7deaf4b35ab9a30d0
Gavin.Fang <goodfriendsfzg@gmail.com>
2012-11-26 18:24:29 +0800

    fix bug when setup project's cscope tags

--- tools/setupproject.sh
+++ tools/setupproject.sh
@@ -2,5 +2,5 @@
 echo "setting ctags...ctags -R --c++-kinds=+p --fields=+iaS --extra=+q"
 ctags -R --c++-kinds=+p --fields=+iaS --extra=+q
 echo "setting cscope...cscope -Rbq"
-find . -name "*.h" -o -name "*.c"-o -name "*.cc" -o -name "*.cpp" -o -name "*.c++" -o -name "*.m" -o -name "*.mm"> cscope.files
+find . -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" -o -name "*.c++" -o -name "*.m" -o -name "*.mm"> cscope.files
 cscope -bq -i cscope.files
